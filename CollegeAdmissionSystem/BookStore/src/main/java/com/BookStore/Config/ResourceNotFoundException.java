f52191187ceb2bfeb90c3702b74405e7e22b1a38
yashbharda <bhardayash981@gmail.com>
2025-07-15 11:40:41 +0530

    Removed extra BookStore folder from CollegeAdmissionSystem

--- CollegeAdmissionSystem/BookStore/src/main/java/com/BookStore/Config/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.BookStore.Config;
-
-public class ResourceNotFoundException extends RuntimeException{
-    public ResourceNotFoundException(String message){
-        super(message);
-    }
-}
